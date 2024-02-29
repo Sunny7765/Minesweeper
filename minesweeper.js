4f5515501226881461bfc094070b1d133977ab01
Sunny Liu <sunnyliu776@gmail.com>
2024-02-29 02:30:17 -0500

    Fixed Minesweeper Grid Generation

--- minesweeper.js
+++ minesweeper.js
@@ -1,5 +1,5 @@
 const gameBox = document.getElementsByClassName("gameBox");
-let rows = document.getElementsByClassName("gridRow");
+let rows = document.getElementsByClassName("gridColumn");
 let cells = document.getElementsByClassName("cell");
 
 console.log(gameBox);
@@ -15,17 +15,28 @@ function defaultGrid() {
 function makeRows(rowNum) {
     for (i = 0; i < rowNum; i++) {
         let row = document.createElement("div");
-        gameBox[0].appendChild(row).className = "gridRow";
-    };
-};
+        gameBox[0].appendChild(row).className = "gridColumn";
+    }
+}
 
 function makeColumns(cellNum) {
     for (j = 0; j < rows.length; j++) {
         for (k = 0; k < cellNum; k++) {
             let newCell = document.createElement("div");
+            newCell.addEventListener("click", function() {
+                newCell.style.backgroundColor = "black";
+            })
             rows[k].appendChild(newCell).className = "cell";
-        };
-    };
-};
+        }
+    }
+}
+
+function makeMines(numMines) {
+    for (m = 0; m < numMines; m++) {
+        let randCell = cells[Math.floor(Math.random() * cells.length)];
+        randCell.textContent = "*";
+    }
+}
 
 defaultGrid();
+makeMines(80)
